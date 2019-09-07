d1419ee395a9360ce6f5c9006974152f9fc0c1b6
Edgar Alejandro <edgar.alejandro@mail.citytech.cuny.edu>
2019-09-07 19:57:54 -0400

    Added Shapes
    I added the Cube and Sphere for my part

---
+++ shapes/Assets/CubeMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Cube Moment is determined with a Tan Function For Y axies and a Cosin function for x axies 
+         transform.position = new Vector3(3 * Mathf.Cos(Time.time), 5 * Mathf.Tan(Time.time*1), 0);
+    }
+}
---
+++ shapes/Assets/SphereMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Line 17 utilizes the function Tangent in order to make the sphere stop twoards the center
+        transform.position = new Vector3(3 * Mathf.Tan(Time.time), 2, 0);
+    }
+}
